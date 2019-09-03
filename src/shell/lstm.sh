f3cfbc87978b6d68b8763033f4a55b90ebe77efb
yoshida <slmnphmet1.2@gmail.com>
2019-09-03 14:55:00 +0900

    prediction by lstm

--- src/shell/lstm.sh
+++ src/shell/lstm.sh
@@ -18,4 +18,12 @@ python src/lstm.py 10 200 1000 linear \
   result/pictures/lstm/len10_neuron200_epoch1000_linear_activtion_fitting.png \
   result/pictures/lstm/len10_neuron200_epoch1000_linear_activtion_pred.png &
 
+python src/lstm.py 5 300 1000 linear \
+  result/pictures/lstm/len5_neuron300_epoch1000_linear_activtion_fitting.png \
+  result/pictures/lstm/len5_neuron300_epoch1000_linear_activtion_pred.png &
+
+python src/lstm.py 10 300 1000 linear \
+  result/pictures/lstm/len10_neuron300_epoch1000_linear_activtion_fitting.png \
+  result/pictures/lstm/len10_neuron300_epoch1000_linear_activtion_pred.png &
+
 wait
